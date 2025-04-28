1ff84753c9bde476b18615255b5845933312fbd4
Vuong Tien Nguyen Bui <vuongtiennguyenbui@gmail.com>
2025-04-29 05:12:44 +0700

    initial commit

---
+++ backend/src/main/java/com/hcmus/softdes/aivideocreator/AivideocreatorApplication.java
@@ -0,0 +1,13 @@
+package com.hcmus.softdes.aivideocreator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AivideocreatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AivideocreatorApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/hcmus/softdes/aivideocreator/AivideocreatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcmus.softdes.aivideocreator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AivideocreatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
