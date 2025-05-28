ccb797306bd27c8cc37cfbbe9fd4727b27548f92
vow130504 <22120344@student.hcmus.edu.vn>
2025-05-28 21:30:29 +0700

    api vid create

---
+++ frontend/src/app/createVideo/page.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import React from "react";
+import Sidebar from "@/features/dashboard/components/Sidebar";
+import "../dashboard/layout.css";
+
+export default function CreateVideoPage() {
+  return (
+    <div style={{ display: "flex", height: "100vh" }}>
+      <Sidebar />
+      <main
+        style={{
+          flex: 1,
+          padding: "5rem 0 0 0",
+          background: "url('/Bg.svg') no-repeat center top",
+          backgroundSize: "cover",
+        }}
+      >
+        
+      </main>
+    </div>
+  );
+}
