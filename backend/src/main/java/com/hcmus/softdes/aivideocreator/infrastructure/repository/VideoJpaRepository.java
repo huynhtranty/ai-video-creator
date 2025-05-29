ffb0a474a3feed00c0aa4748d9bfcf17d221daff
hytaty <huynhtranty@gmail.com>
2025-05-29 22:17:00 +0700

    fix jpa video file

--- backend/src/main/java/com/hcmus/softdes/aivideocreator/infrastructure/repository/VideoJpaRepository.java
+++ backend/src/main/java/com/hcmus/softdes/aivideocreator/infrastructure/repository/VideoJpaRepository.java
@@ -9,6 +9,6 @@ import java.util.List;
 import java.util.UUID;
 
 public interface VideoJpaRepository extends JpaRepository<VideoEntity, UUID> {
-     List<Video> findByUserId(UUID userId);
-     Collection<Object> findByProjectId(UUID projectId);
+     List<VideoEntity> findByUserId(UUID userId);
+     List<VideoEntity> findByProjectId(UUID projectId);
 }
