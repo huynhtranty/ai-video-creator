dae1756ad2fb8968bfaa784b578a67b2765b8f70
vow130504 <22120344@student.hcmus.edu.vn>
2025-06-30 20:38:25 +0700

    add statistic btn

---
+++ frontend/src/app/statistic/page.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import React from "react";
+import Sidebar from "@/features/dashboard/components/Sidebar";
+
+export default function ListProjectPage() {
+  return (
+    <div style={{ display: "flex", height: "100vh" }}>
+      <Sidebar />
+      <main
+        style={{
+          flex: 1,
+          padding: "5rem 0 0 0",
+          background: "url('/Bg.svg') no-repeat center top",
+          backgroundSize: "cover",
+        }}
+      >
+        
+      </main>
+    </div>
+  );
+}
