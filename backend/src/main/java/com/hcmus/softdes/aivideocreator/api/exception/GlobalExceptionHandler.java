019da409eda3eea2ccfaaf53c69babff505def30
Huynh Tran Ty <22120418@student.hcmus.edu.vn>
2025-07-11 21:00:04 +0700

    refactor

--- backend/src/main/java/com/hcmus/softdes/aivideocreator/api/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/hcmus/softdes/aivideocreator/api/exception/GlobalExceptionHandler.java
@@ -11,14 +11,14 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class GlobalExceptionHandler {
 
     // Custom exception for handling specific cases
-//    @ExceptionHandler(NotFoundException.class)
-//    public ProblemDetail handleNotFoundException(NotFoundException e) {
-//        return ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, e.getMessage());
-//    }
-//
-//
-//    @ExceptionHandler(Exception.class)
-//    public ProblemDetail handleGenericException(Exception e) {
-//        return ProblemDetail.forStatusAndDetail(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
-//    }
+    @ExceptionHandler(NotFoundException.class)
+    public ProblemDetail handleNotFoundException(NotFoundException e) {
+        return ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, e.getMessage());
+    }
+
+
+    @ExceptionHandler(Exception.class)
+    public ProblemDetail handleGenericException(Exception e) {
+        return ProblemDetail.forStatusAndDetail(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
+    }
 }
